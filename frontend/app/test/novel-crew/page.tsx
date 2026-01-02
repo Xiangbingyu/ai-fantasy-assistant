c9d41601b333d5ef1c06a661c49f25d0064f382f
Xiangbingyu <2473679079@qq.com>
2026-01-02 14:37:49 +0800

    feat: 添加小说团队功能测试页面

---
+++ frontend/app/test/novel-crew/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function NovelCrewTestPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Novel Crew Test Page</h1>
+      <p>This is a test page for novel crew functionality.</p>
+    </div>
+  );
+}
